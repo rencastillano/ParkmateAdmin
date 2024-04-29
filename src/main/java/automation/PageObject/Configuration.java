9a7da57b42df2432b6a4f4b7d857a7b6843eac2f
renier <ren.castillano@smsupermalls.com>
2024-04-29 13:38:43 +0800

    configuration module setup

--- src/main/java/automation/PageObject/Configuration.java
+++ src/main/java/automation/PageObject/Configuration.java
@@ -1,6 +1,8 @@
 package automation.PageObject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import automation.AbstractComponents.AbstractComponent;
@@ -16,4 +18,10 @@ public class Configuration extends AbstractComponent{
 	}
 
 	// PageFactory
+	
+	@FindBy(css = "a:nth-child(1) > span:nth-child(2)")
+	WebElement configurationModule;
+	
+	@FindBy(xpath = "//a[@href='/configuration/parkers']//div[@class='py-5 self-center']//*[name()='svg']")
+	WebElement parkerEnrollement;
 }
